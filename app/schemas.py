79a21c75bd829194f67e51b3a55d97947d5bc609
smirnovds1990 <smirnovds1990@gmail.com>
2025-07-22 12:22:40 +0300

    add Pydantic to all model interactions

--- app/schemas.py
+++ app/schemas.py
@@ -50,3 +50,47 @@ class ProductSchema(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class CategoryInSchema(BaseModel):
+    Category_ID: int
+    Category_Name: str
+    Category_Image: str | None
+    sort_order: int | None
+
+
+class ProductMarkInSchema(BaseModel):
+    Mark_ID: int
+    Mark_Name: str
+
+
+class ImageInSchema(BaseModel):
+    Image_ID: int
+    Image_URL: str
+    MainImage: bool
+    Product_ID: int
+    position: int | str | None
+    sort_order: int | None
+    title: str | None
+
+
+class ParameterInSchema(BaseModel):
+    Parameter_ID: int
+    chosen: bool | None
+    disabled: bool | None
+    extra_field_color: str | None
+    extra_field_image: str | None
+    name: str | None
+    old_price: Decimal | None
+    parameter_string: str | None
+    price: Decimal | None
+    sort_order: int | None
+
+
+class ProductInSchema(BaseModel):
+    Product_ID: int
+    Product_Name: str
+    OnMain: bool
+    images: list[ImageInSchema]
+    parameters: list[ParameterInSchema]
+    categories: list[CategoryInSchema]
