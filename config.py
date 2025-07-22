5cd594fa37ed737385618b2309529217b048daee
smirnovds1990 <smirnovds1990@gmail.com>
2025-07-22 13:05:08 +0300

    add containerization via docker compose

--- config.py
+++ config.py
@@ -9,5 +9,13 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     DEBUG = bool(os.getenv("DEBUG"))
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_DB = os.getenv("POSTGRES_DB")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = int(os.getenv("DB_PORT", 5432))
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@"
+        f"{DB_HOST}:{DB_PORT}/{POSTGRES_DB}"
+    )
     FETCH_INTERVAL = int(os.getenv("FETCH_INTERVAL", 300))
--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5555, threaded=True)
+    app.run(host="0.0.0.0", port=5555, threaded=True)
